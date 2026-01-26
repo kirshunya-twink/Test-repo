4e897b57feed43172eeff74abeb0dbd4c2f959a0
Nikita <bakunovich.nikita@mail.ru>
2026-01-26 16:19:34 +0300

    some fixes

--- internal/handler/health.go
+++ internal/handler/health.go
@@ -1,8 +1,13 @@
 package handler
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func Health(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("ok"))
+	if _, err := w.Write([]byte("ok")); err != nil {
+		log.Printf("failed to write response: %v", err)
+	}
 }
