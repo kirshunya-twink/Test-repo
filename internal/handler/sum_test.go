e9403c0191e970a652679f8a000f88c3d858a64d
Nikita <bakunovich.nikita@mail.ru>
2026-01-28 15:45:04 +0300

    Unit test for sum fixed.

--- internal/handler/sum_test.go
+++ internal/handler/sum_test.go
@@ -6,14 +6,23 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/gin-gonic/gin"
 )
 
 func TestSum(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	w := httptest.NewRecorder()
+	_, r := gin.CreateTestContext(w)
+
+	r.POST("/sum", Sum)
+
 	body := []byte(`{"a":2,"b":3}`)
 	req := httptest.NewRequest(http.MethodPost, "/sum", bytes.NewBuffer(body))
-	w := httptest.NewRecorder()
+	req.Header.Set("Content-Type", "application/json")
 
-	Sum(w, req)
+	r.ServeHTTP(w, req)
 
 	if w.Code != http.StatusOK {
 		t.Fatalf("expected 200, got %d", w.Code)
